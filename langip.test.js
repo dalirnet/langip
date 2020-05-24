61b19cc576fbfef17da90dd2eb340af2dd4a17a7
AmirRezaDalir <dalirnet@gmail.com>
2020-05-25 00:42:48 +0430

    rename

--- langip.test.js
+++ langip.test.js
@@ -1,11 +1,11 @@
 const _ = require("lodash");
-const irip = require("./index");
+const langip = require("./index");
 const { languages } = require("./src/config.json");
 
 _.forEach(languages, (value, key) => {
   test(`utility with database [${key}]`, () => {
     const [[start, end]] = require(`./db/${key}.json`);
-    expect(irip.utility.ipTolong(irip.utility.longToip(start))).toBe(start);
-    expect(irip.utility.ipTolong(irip.utility.longToip(end))).toBe(end);
+    expect(langip.utility.ipTolong(langip.utility.longToip(start))).toBe(start);
+    expect(langip.utility.ipTolong(langip.utility.longToip(end))).toBe(end);
   });
 });
